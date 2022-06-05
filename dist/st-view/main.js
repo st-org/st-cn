3351b6b5b2adbaaa31f43e5a717eaa120ba612b6
DDU <ddu6@protonmail.com>
2022-06-06 03:08:06 +0800

    update

--- dist/st-view/main.js
+++ dist/st-view/main.js
@@ -1,4 +1,7 @@
+/*
 import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.12/mod.js'
+*/
+import {init} from '../stui@0.15.12/mod.js'
 import {createViewer} from './mod.js'
 init()
 const style = document.createElement('style')
